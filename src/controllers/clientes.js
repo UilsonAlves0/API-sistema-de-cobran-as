7d14ca53cf5b244d1445b57a0180fda7582104c3
Uilson Alves <uilsonneto2001@hotmail.com>
2020-11-19 07:42:55 -0300

    feat: Adicionando o end point de editar cliente

--- src/controllers/clientes.js
+++ src/controllers/clientes.js
@@ -1,6 +1,7 @@
 const verificacao = require('../repositories/verificacao');
 const cadastros = require('../repositories/cadastros');
 const response = require('./response');
+const cliente = require('../repositories/clientes');
 
 const criarCliente = async (ctx) => {
 	const {
@@ -25,4 +26,67 @@ const criarCliente = async (ctx) => {
 	);
 	return response(ctx, 201, { idCustomer });
 };
-module.exports = { criarCliente };
+
+const editarCliente = async (ctx) => {
+	const {
+		id = null,
+		nome = null,
+		cpf = null,
+		email = null,
+	} = ctx.request.body;
+	if (!id || !nome || !cpf || !email) {
+		return response(ctx, 400, { mensagem: 'Requisição mal formulada' });
+	}
+	const clienteEditado = await cliente.editClient(nome, cpf, email, id);
+	if (clienteEditado) {
+		return response(ctx, 200, { clienteEditado });
+	}
+	return response(ctx, 200, { mensagem: 'Pedido mal formulado' });
+};
+
+const getClientes = async (ctx) => {
+	const busca = ctx.query.busca;
+	const clientesPorPagina = ctx.request.query.clientesPorPagina;
+	const offset = ctx.request.query.offset;
+
+	if (busca) {
+		if (!busca || !clientesPorPagina || !offset) {
+			return response(ctx, 400, { mensagem: 'Requisição mal formulada' });
+		}
+		const clientesDB = await cliente.getClients(
+			busca,
+			offset,
+			clientesPorPagina
+		);
+		const clientesBusca = clientesDB.map((client) => {
+			return {
+				nome: client.nome,
+				email: client.email,
+				cobrancasFeitas: 2000,
+				cobrancasRecebidas: 10000,
+				estaInadiplente: true,
+			};
+		});
+		return response(ctx, 200, { clientesBusca });
+	}
+	if (!clientesPorPagina || !offset) {
+		return response(ctx, 400, { mensagem: 'Requisição mal formulada' });
+	}
+	const clientesDB = await cliente.listClients(clientesPorPagina, offset);
+	const clientes = clientesDB.map((client) => {
+		return {
+			nome: client.nome,
+			email: client.email,
+			cobrancasFeitas: 2000,
+			cobrancasRecebidas: 10000,
+			estaInadiplente: true,
+		};
+	});
+	return response(ctx, 200, { clientes });
+};
+
+module.exports = {
+	criarCliente,
+	editarCliente,
+	getClientes,
+};
